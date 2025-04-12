870d52648197fcbbd5b4b0ffb092be52429673d3
kimju <op45672@gmail.com>
2025-04-12 18:32:11 +0900

    개새끼야

--- src/main/java/test_spring/test/TestApplication.java
+++ src/main/java/test_spring/test/TestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TestApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
