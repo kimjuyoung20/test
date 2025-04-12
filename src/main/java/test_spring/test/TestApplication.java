fc4f903b2940fc0cf9b292424ceef61d8bf53757
kimju <op45672@gmail.com>
2025-04-12 18:43:04 +0900

    개새끼야

--- src/main/java/test_spring/test/TestApplication.java
+++ src/main/java/test_spring/test/TestApplication.java
@@ -1,5 +1,6 @@
 package test_spring.test;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,7 @@ public class TestApplication {
 		SpringApplication.run(TestApplication.class, args);
 		System.out.println("Hello World!");
 		System.out.println("개 좆같은 깃허브");
+		System.out.println("temp");
 	}
 
 }
